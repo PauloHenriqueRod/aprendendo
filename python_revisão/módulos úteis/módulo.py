c3f0d0517dc7fa6baa64ffa1f152a66844e8bb18
Paulo Henrique Rodrigues de Araujo <phrod.devp@gmail.com>
2022-03-10 14:59:44 -0300

    módulos úteis

---
+++ python_revisão/módulos úteis/módulo.py
@@ -0,0 +1,12 @@
+# import pygame
+# pygame.mixer.init()
+# pygame.init()
+# pygame.mixer.music.load('enemy.mp3')
+# pygame.mixer.music.play()
+# pygame.event.wait()
+# pygame.mixer.music.load('roots.mp3')
+# pygame.mixer.music.play()
+# pygame.event.wait()
+
+
+# QUANDO FEITO NO PYCHARM, O MÓDULO É BAIXADO AUTOMATICAMENTE
