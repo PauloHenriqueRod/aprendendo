31016d64251962b7445425842181694ebf390ea9
Paulo Henrique Rodrigues de Araujo <phrod.devp@gmail.com>
2022-03-10 15:19:10 -0300

    adicionando

--- python_revisão/simbolos_matematicos.py
+++ python_revisão/simbolos_matematicos.py
@@ -6,9 +6,18 @@ print(f'23//4={23//4}')
 print(f'23*4={23*4}')
 print(f'23**4={23**4}')
 print(f'23%4={23%4}')
+print('='*30)
 
+# format(valor, [=, b, o, x, d, E, X])
+# = -> COLOCA O SINAL DE IGUAL MAIS À ESQUERDA
+# b -> CONVERTE O VALOR EM BINÁRIO 
+# o -> TRANSFORMA PARA O FORMATO OCTAL
+# x -> CONVERTO PARA O VALOR HEXADECIMAL
+# E -> FORMATO CIENTÍFICO
+# X -> FORMATO HEXADECIMAL COM LETRAS MAIÚSCULAS
 
-# .isalpha = saber se é alfabético
-# .isspace = se tem espaçoes
-# .isupper = se tá em maiúsculas
-# .islower = se tá em minúsculas
+print(format(27, 'b'))
+print(format(27, 'o'))
+print(format(27, 'x'))
+print(format(27, 'E'))
+print(format(27, 'X'))
