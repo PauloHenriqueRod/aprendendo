0ed562f607c45656f8596f688b78b5213a2a0df1
Paulo Henrique Rodrigues de Araujo <phrod.devp@gmail.com>
2022-03-12 17:59:27 -0300

    Create revisão_lista.py

---
+++ python_revisão/revisão_lista.py
@@ -0,0 +1,27 @@
+lista = ['Fluminense', 'Flamengo', 'Vasco', 'Botafogo']
+
+# FUNÇÃO len(lista) CONTA O NÚMERO DE ELEMENTOS DA LISTA
+print(len(lista))
+print('=-'*30)
+
+# max(lista), min(lista) -> ENCONTRA O MAIOR VALOR DA LISTA
+lista_numérica = [1, 8, 4, 7, 16, 3]
+print(max(lista_numérica))
+# NO CASO DE LISTA COM APENAS PALAVRAS, ENCONTRA O ÚLTIMO ITEM EM ORDEM ALFABÉTICA
+print(max(lista))
+print('=-'*30)
+
+# lista.append -> ADICIONA UM ITEM NA LISTA
+# lista.insert(pos, x) -> ADICIONA UM ITEM NA POSIÇÃO X
+lista.append('Corinthians')
+print(lista)
+lista.insert(2, 'Corissabá')
+print(lista)
+print('=-'*30)
+
+# FUNÇÃO lista.index ENCONTRA A POSIÇÃO DO ITEM ESCOLHIDO, EM CASO DE NÃO EXISTIR O ITEM NA LISTA RETORNA A POSIÇÃO "-1"
+print(lista.index('Fluminense'))
+print('=-'*30)
+
+# FUNÇÃO lista.count CONTA O NÚMERO DE VEZES QUE UM CERTO ELEMENTO APARECE NA LISTA
+print(lista.count('Fluminense'))
