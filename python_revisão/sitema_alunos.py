b850f07de22a88f389a0d256313cdfa63ee6e3a8
Paulo Henrique Rodrigues de Araujo <phrod.devp@gmail.com>
2022-03-14 16:19:49 -0300

    Create sitema_alunos.py

---
+++ python_revisão/sitema_alunos.py
@@ -0,0 +1,16 @@
+aluno = list()
+lista_alunos = list()
+while True:
+    nome = str(input('NOME DO ALUNO: '))
+    if nome == '':
+        break
+    nota1 = str(input('NOTA 1:')).replace(',', '.').strip()
+    nota2 = str(input('NOTA 2: ')).replace(',', '.').strip()
+    aluno.append(nome)
+    aluno.append(float(nota1))
+    aluno.append(float(nota2))
+    lista_alunos.append(aluno[:])
+    aluno.clear()
+print('{:.<5}{:.<15}{:.>10}'.format('Nº', 'Nome', 'Média'))
+for c in range(0, len(lista_alunos)):
+    print(f'{c+1:<5}{lista_alunos[c][0]}{(lista_alunos[c][1]+lista_alunos[c][2])/2:>10.1f}')
