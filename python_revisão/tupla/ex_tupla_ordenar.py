14232281b259c0be632d7d8e10a8780787aaece6
Paulo Henrique Rodrigues de Araujo <phrod.devp@gmail.com>
2022-03-15 10:46:00 -0300

    Update ex_tupla_ordenar.py

--- python_revisão/tupla/ex_tupla_ordenar.py
+++ python_revisão/tupla/ex_tupla_ordenar.py
@@ -13,3 +13,20 @@ ranking = sorted(jogos.items(), key=itemgetter(1), reverse=True)
 for q, r in enumerate(ranking):
     print(f'{q+1}º lugar - {r[0]} - {r[1]}')
     sleep(0.5)
+
+# ----------------- OUTRO MODO DE FAZER -----------------
+
+# from random import randint
+# from time import sleep
+# pos = 1
+# jog = {'Jogador 1': randint(1,6),
+#      'Jogador 2': randint(1,6),
+#      'Jogador 3': randint(1,6),
+#      'Jogador 4': randint(1,6)}
+# for k, v in jog.items():
+#     print(f'O {k} tirou {v}')
+#     sleep(0.8)
+# b = sorted(jog, key=jog.get, reverse=True)
+# for i in sorted(jog,key=jog.get,reverse=True):
+#     print(f'{pos}º- {i}: tirou {jog[i]}')
+#     pos += 1
