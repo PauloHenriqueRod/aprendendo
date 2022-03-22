3f04ae3dd98d5c63c199a8aa721938058fb26b25
Paulo Henrique Rodrigues de Araujo <phrod.devp@gmail.com>
2022-03-22 15:41:32 -0300

    Create apis.py

---
+++ projetos uteis/apis.py
@@ -0,0 +1,1 @@
+# APIS: Quando o programa precisa consumir dados de terceiros, muitas vezes precisa-se acessar na internet dados para serem usados em sua aplicação. A linguagem usada para traduzir esses dados para os aplicativos é chamada de APIS.
