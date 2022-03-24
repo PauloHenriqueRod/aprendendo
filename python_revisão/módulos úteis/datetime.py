1d775d2adc766ca5c808e97fdd56709e0d8226da
Paulo Henrique Rodrigues de Araujo <phrod.devp@gmail.com>
2022-03-24 14:55:17 -0300

    atualizacao

--- python_revisão/módulos úteis/datetime.py
+++ python_revisão/módulos úteis/datetime.py
@@ -1,13 +1,13 @@
-import datetime
+from datetime import date, timedelta, datetime
 
-d = datetime.date(2001, 10, 25)
-hoje = datetime.date.today()
-timedelta = datetime.timedelta(hours=12)
+d = date(2001, 10, 25)
+hoje = date.today()
+timedelta = timedelta(hours=12)
 print(hoje + timedelta)
 
-outro_dia = datetime.date(2020, 2, 15)
+outro_dia = date(2020, 2, 15)
 diferenca_dia = outro_dia - hoje
 print(diferenca_dia)
 
-data_agora = datetime.datetime.now()
+data_agora = datetime.now()
 print(data_agora.strftime('%B %d, %Y'))
