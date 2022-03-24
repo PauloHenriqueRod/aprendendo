1d89a9b54fc9cc413089e48f06f9d9a2c3641e7d
Paulo Henrique Rodrigues de Araujo <phrod.devp@gmail.com>
2022-03-24 15:32:11 -0300

    teste de módulos

---
+++ python_revisão/módulos úteis/modulos.py
@@ -0,0 +1,7 @@
+def soma(x, y):
+    return x+y
+
+# FAZER ISSO PARA TESTAR A FUNÇÃO
+if __name__ == '__main__':
+    print(soma(8, 7))
+    print(soma(7, 53))
---
+++ python_revisão/módulos úteis/modulos_teste.py
@@ -0,0 +1,4 @@
+from modulos import soma
+
+print(soma(7, 3))
+print(soma(5, 1))
